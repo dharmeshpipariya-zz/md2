a3131d201eb563f9e666394388568066a9c2dac1
dharmeshpipariya <pipariyadharmesh@gmail.com>
2016-04-29 19:02:05 +0530

    update

--- src/components/switch/switch.ts
+++ src/components/switch/switch.ts
@@ -13,23 +13,19 @@ let nextId = 0;
  * Provider Expression that allows md-checkbox to register as a ControlValueAccessor. This allows it
  * to support [(ngModel)] and ngControl.
  */
-const MD_SWITCH_CONTROL_VALUE_ACCESSOR = CONST_EXPR(new Provider(
+const MD_SWITCH_CONTROL_VALUE_ACCESSOR = CONST_EXPR( new Provider(
     NG_VALUE_ACCESSOR, {
-        useExisting: forwardRef(() => Md2Switch),
+        useExisting: forwardRef(() => Md2Switch ),
         multi: true
-    }));
+    }) );
 
 /**
  * Represents the different states that require custom transitions between them.
  */
 enum TransitionCheckState {
-    /** The initial state of the component before any user interaction. */
     Init,
-    /** The state representing the component when it's becoming checked. */
     Checked,
-    /** The state representing the component when it's becoming unchecked. */
     Unchecked,
-    /** The state representing the component when it's becoming indeterminate. */
     Indeterminate
 }
 
@@ -41,103 +37,38 @@ enum TransitionCheckState {
  * have the checkbox be accessible, you may supply an [aria-label] input.
  * See: https://www.google.com/design/spec/components/selection-controls.html
  */
-@Component({
+@Component( {
     selector: 'md2-switch',
-    template: `<div class="md2-switch-layout">
-<div class="md2-switch-container">
+    template: `
+        <div class="md2-switch-layout">
+            <div class="md2-switch-container">
                 <div class="md2-switch-bar"></div>
                 <div class="md2-switch-thumb-container">
-                  <div class="md2-switch-thumb"></div>
+                    <div class="md2-switch-thumb"></div>
                 </div>
             </div>
-<label [id]="labelId">
-    <ng-content></ng-content>
-  </label>
-</div>`,
+            <label [id]="labelId">
+                <ng-content></ng-content>
+            </label>
+        </div>
+    `,
     styles: [`
-.md2-switch-layout{margin: 16px;
-    margin-left: inherit;
-    white-space: nowrap;
-    cursor: pointer;
-    outline: 0;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    min-height: 30px;
-    line-height: 28px;
-    -webkit-align-items: center;
-    -ms-flex-align: center;
-    align-items: center;
-    display: -webkit-flex;
-    display: -ms-flexbox;
-    display: flex;}
-.md2-switch-layout label{border-color: transparent;
-    border-width: 0;
-cursor: pointer;
-    float: left;word-wrap: break-word;
-    max-width: 100%;
-    white-space: normal;line-height: normal;}
-.md-switch:focus {
-  outline: none; }
-.md-switch .md2-switch-container {
-display: inline-block;
-  cursor: pointer;
-  width: 36px;
-min-width: 36px;
-  height: 24px;
-  position: relative;
-  user-select: none;
-  margin-right: 8px; }
-.md-switch.md-switch-disabled .md2-switch-container {
-  cursor: not-allowed; }
-.md-switch.md-switch-disabled .md2-switch-bar {
-  background-color: rgba(0, 0, 0, 0.12); }
-.md-switch.md-switch-disabled .md2-switch-thumb {
-  background-color: #bdbdbd; }
-.md-switch .md2-switch-bar {
-  left: 1px;
-  width: 34px;
-  top: 5px;
-  height: 14px;
-  border-radius: 8px;
-  position: absolute;
-  background-color: #9e9e9e; }
-.md-switch.md-switch-checked .md2-switch-bar {
-  background-color: rgba(33, 150, 243, 0.5); }
-.md-switch.md-switch-checked .md2-switch-thumb-container {
-  transform: translate3d(100%, 0, 0); }
-.md-switch.md-switch-checked .md2-switch-thumb {
-  background-color: #2196f3; }
-.md-switch .md2-switch-thumb-container {
-  top: 2px;
-  left: 0;
-  width: 16px;
-  position: absolute;
-  transform: translate3d(0, 0, 0);
-  z-index: 1; }
-.md-switch .md2-switch-thumb {
-  position: absolute;
-  margin: 0;
-  left: 0;
-  top: 0;
-  outline: none;
-  height: 20px;
-  width: 20px;
-  border-radius: 50%;
-  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.26);
-  background-color: #fafafa; }
-.md-switch:not(.md-switch-dragging) .md2-switch-bar {
-  transition-delay: 0.05s;
-  transition: all 0.08s linear;
-  transition-property: transform, background-color; }
-.md-switch:not(.md-switch-dragging) .md2-switch-thumb {
-  transition-delay: 0.05s;
-  transition: all 0.08s linear;
-  transition-property: transform, background-color; }
-.md-switch:not(.md-switch-dragging) .md2-switch-thumb-container {
-  transition: all 0.08s linear;
-  transition-property: transform, background-color; }
+        .md2-switch-layout { margin: 16px; margin-left: inherit; white-space: nowrap; cursor: pointer; outline: 0; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; min-height: 30px; line-height: 28px; -webkit-align-items: center; -ms-flex-align: center; align-items: center; display: -webkit-flex; display: -ms-flexbox; display: flex; }
+        .md2-switch-layout label { border-color: transparent; border-width: 0; cursor: pointer; float: left; word-wrap: break-word; max-width: 100%; white-space: normal; line-height: normal; }
+        .md-switch:focus { outline: none; }
+        .md-switch .md2-switch-container { display: inline-block; cursor: pointer; width: 36px; min-width: 36px; height: 24px; position: relative; user-select: none; margin-right: 8px; }
+        .md-switch.md-switch-disabled .md2-switch-container { cursor: not-allowed; }
+        .md-switch.md-switch-disabled .md2-switch-bar { background-color: rgba(0, 0, 0, 0.12); }
+        .md-switch.md-switch-disabled .md2-switch-thumb { background-color: #bdbdbd; }
+        .md-switch .md2-switch-bar { left: 1px; width: 34px; top: 5px; height: 14px; border-radius: 8px; position: absolute; background-color: #9e9e9e; }
+        .md-switch.md-switch-checked .md2-switch-bar { background-color: rgba(33, 150, 243, 0.5); }
+        .md-switch.md-switch-checked .md2-switch-thumb-container { transform: translate3d(100%, 0, 0); }
+        .md-switch.md-switch-checked .md2-switch-thumb { background-color: #2196f3; }
+        .md-switch .md2-switch-thumb-container { top: 2px; left: 0; width: 16px; position: absolute; transform: translate3d(0, 0, 0); z-index: 1; }
+        .md-switch .md2-switch-thumb { position: absolute; margin: 0; left: 0; top: 0; outline: none; height: 20px; width: 20px; border-radius: 50%; box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.26); background-color: #fafafa; }
+        .md-switch:not(.md-switch-dragging) .md2-switch-bar { transition-delay: 0.05s; transition: all 0.08s linear; transition-property: transform, background-color; }
+        .md-switch:not(.md-switch-dragging) .md2-switch-thumb { transition-delay: 0.05s; transition: all 0.08s linear; transition-property: transform, background-color; }
+        .md-switch:not(.md-switch-dragging) .md2-switch-thumb-container { transition: all 0.08s linear; transition-property: transform, background-color; }
     `],
     host: {
         'role': 'checkbox',
@@ -165,7 +96,7 @@ export class Md2Switch implements ControlValueAccessor {
      * Attached to the aria-label attribute of the host element. In most cases, arial-labelledby will
      * take precedence so this may be omitted.
      */
-    @Input('aria-label') ariaLabel: string = '';
+    @Input( 'aria-label' ) ariaLabel: string = '';
 
     /** A unique id for the checkbox. If one is not supplied, it is auto-generated. */
     @Input() id: string = `md-switch-${++nextId}`;
@@ -201,7 +132,7 @@ export class Md2Switch implements ControlValueAccessor {
 
     private _changeSubscription: { unsubscribe: () => any } = null;
 
-    constructor(private _renderer: Renderer, private _elementRef: ElementRef) { }
+    constructor( private _renderer: Renderer, private _elementRef: ElementRef ) { }
 
     /**
      * Whether the checkbox is checked. Note that setting `checked` will immediately set
@@ -211,12 +142,12 @@ export class Md2Switch implements ControlValueAccessor {
         return this._checked;
     }
 
-    set checked(checked: boolean) {
+    set checked( checked: boolean ) {
         this._indeterminate = false;
         this._checked = checked;
         this._transitionCheckState(
-            this._checked ? TransitionCheckState.Checked : TransitionCheckState.Unchecked);
-        this.change.emit(this._checked);
+            this._checked ? TransitionCheckState.Checked : TransitionCheckState.Unchecked );
+        this.change.emit( this._checked );
     }
 
     /**
@@ -232,13 +163,13 @@ export class Md2Switch implements ControlValueAccessor {
         return this._indeterminate;
     }
 
-    set indeterminate(indeterminate: boolean) {
+    set indeterminate( indeterminate: boolean ) {
         this._indeterminate = indeterminate;
-        if (this._indeterminate) {
-            this._transitionCheckState(TransitionCheckState.Indeterminate);
+        if ( this._indeterminate ) {
+            this._transitionCheckState( TransitionCheckState.Indeterminate );
         } else {
             this._transitionCheckState(
-                this.checked ? TransitionCheckState.Checked : TransitionCheckState.Unchecked);
+                this.checked ? TransitionCheckState.Checked : TransitionCheckState.Unchecked );
         }
     }
 
@@ -249,7 +180,7 @@ export class Md2Switch implements ControlValueAccessor {
 
     /** Returns the proper aria-checked attribute value based on the checkbox's state. */
     getAriaChecked() {
-        if (this.indeterminate) {
+        if ( this.indeterminate ) {
             return 'mixed';
         }
         return this.checked ? 'true' : 'false';
@@ -263,8 +194,8 @@ export class Md2Switch implements ControlValueAccessor {
     /**
      * Event handler used for both (click) and (keyup.space) events. Delegates to toggle().
      */
-    onInteractionEvent(event: Event) {
-        if (this.disabled) {
+    onInteractionEvent( event: Event ) {
+        if ( this.disabled ) {
             event.stopPropagation();
             return;
         }
@@ -272,43 +203,43 @@ export class Md2Switch implements ControlValueAccessor {
     }
 
     /** Implemented as part of ControlValueAccessor. */
-    writeValue(value: any) {
+    writeValue( value: any ) {
         this.checked = !!value;
     }
 
     /** Implemented as part of ControlValueAccessor. */
-    registerOnChange(fn: any) {
-        if (this._changeSubscription) {
+    registerOnChange( fn: any ) {
+        if ( this._changeSubscription ) {
             this._changeSubscription.unsubscribe();
         }
-        this._changeSubscription = <{ unsubscribe: () => any }>this.change.subscribe(fn);
+        this._changeSubscription = <{ unsubscribe: () => any }>this.change.subscribe( fn );
     }
 
     /** Implemented as part of ControlValueAccessor. */
-    registerOnTouched(fn: any) {
+    registerOnTouched( fn: any ) {
         this.onTouched = fn;
     }
 
-    private _transitionCheckState(newState: TransitionCheckState) {
+    private _transitionCheckState( newState: TransitionCheckState ) {
         let oldState = this._currentCheckState;
         let renderer = this._renderer;
         let elementRef = this._elementRef;
 
-        if (oldState === newState) {
+        if ( oldState === newState ) {
             return;
         }
 
         this._currentAnimationClass = this._getAnimationClassForCheckStateTransition(
-            oldState, newState);
+            oldState, newState );
         this._currentCheckState = newState;
 
     }
 
     private _getAnimationClassForCheckStateTransition(
-        oldState: TransitionCheckState, newState: TransitionCheckState): string {
+        oldState: TransitionCheckState, newState: TransitionCheckState ): string {
         var animSuffix: string;
 
-        switch (oldState) {
+        switch ( oldState ) {
             case TransitionCheckState.Init:
                 return '';
             case TransitionCheckState.Unchecked:
