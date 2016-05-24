3ccfa023d222bc8fdd826ce16e7fb415363822fc
dharmeshpipariya <pipariyadharmesh@gmail.com>
2016-05-24 14:12:55 +0530

    update path of colorpicker service.

--- app/main.js
+++ app/main.js
@@ -4,7 +4,7 @@ const platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
 const core_1 = require("@angular/core");
 const common_1 = require("@angular/common");
 const router_deprecated_1 = require("@angular/router-deprecated");
-const colorpicker_service_1 = require('/components/colorpicker/colorpicker.service');
+const colorpicker_service_1 = require('/md2/components/colorpicker/colorpicker.service');
 platform_browser_dynamic_1.bootstrap(app_1.AppComponent, [
     router_deprecated_1.ROUTER_PROVIDERS,
     core_1.provide(common_1.LocationStrategy, { useClass: common_1.HashLocationStrategy }), colorpicker_service_1.ColorpickerService
