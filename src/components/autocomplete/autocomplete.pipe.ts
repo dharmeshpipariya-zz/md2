ec7ae2c1584a527f23114a3505835d7b900a98ed
dharmeshpipariya <pipariyadharmesh@gmail.com>
2016-05-31 16:03:00 +0530

    fixed: filter issue while filtering values.

--- src/components/autocomplete/autocomplete.pipe.ts
+++ src/components/autocomplete/autocomplete.pipe.ts
@@ -1,15 +1,10 @@
 import {Pipe, PipeTransform} from '@angular/core';
 
-@Pipe({
-  name: 'hightlight'
-})
+@Pipe({ name: 'hightlight' })
 export class HightlightPipe implements PipeTransform {
-  transform(value: string, args: any[]) {
-    if (args.length < 1) { return value; }
-    let query = args[0];
-    return query ?
-      value.replace(new RegExp(this.escapeRegexp(query), 'gi'), '<span class="highlight">$&</span>') :
-      value;
+  transform(value: string, query: string) {
+    if (query.length < 1) { return value; }
+    return query ? value.replace(new RegExp(this.escapeRegexp(query), 'gi'), '<span class="highlight">$&</span>') : value;
   }
   private escapeRegexp(queryToEscape: string) {
     return queryToEscape.replace(/([.?*+^$[\]\\(){}|-])/g, '\\$1');
