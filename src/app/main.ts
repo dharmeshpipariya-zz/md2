c8a9731cd855613d9b23c63bdfab8f914249397f
dharmeshpipariya <pipariyadharmesh@gmail.com>
2016-07-21 19:04:02 +0530

    fixed dialog provider issue.

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,8 +3,9 @@ import {bootstrap} from "@angular/platform-browser-dynamic";
 import {provide} from "@angular/core";
 import {LocationStrategy, PathLocationStrategy} from "@angular/common";
 import {ROUTER_PROVIDERS} from "@angular/router-deprecated";
+import {DIALOG_PROVIDERS}from '../components/dialog/dialog'
 
-bootstrap(AppComponent, [
-  ROUTER_PROVIDERS,
-  provide(LocationStrategy, { useClass: PathLocationStrategy })
-]);
+bootstrap( AppComponent, [
+  ROUTER_PROVIDERS, DIALOG_PROVIDERS,
+  provide( LocationStrategy, { useClass: PathLocationStrategy })
+] );
