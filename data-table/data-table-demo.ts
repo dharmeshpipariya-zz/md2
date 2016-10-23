94c18b3513be0116990ad27ee0babc830da00576
Dharmesh Pipariya <pipariyadharmesh@gmail.com>
2016-10-24 00:44:44 +0530

    update demo

---
+++ data-table/data-table-demo.ts
@@ -0,0 +1,33 @@
+import {
+  Component,
+  Pipe,
+  PipeTransform
+} from '@angular/core';
+import { Http } from "@angular/http";
+
+@Pipe({ name: "dataPipe" })
+export class DataTablePipe implements PipeTransform {
+  transform(array: any[], query: string): any {
+    if (query) {
+      return array.filter((value: any, index: number, arr: any) => value.name.indexOf(query) > -1);
+    }
+    return array;
+  }
+}
+
+@Component({
+  moduleId: module.id,
+  selector: 'data-table-demo',
+  templateUrl: 'data-table-demo.html'
+})
+export class DataTableDemo {
+  private data: any = null;
+  private search: string = null;
+
+  constructor(private http: Http) {
+    http.get("./data-table/data.json")
+      .subscribe((data) => {
+        this.data = data.json();
+      });
+  }
+}
