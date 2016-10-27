1dd3b0d8a909d3657271f4da12ddae5135849e05
dharmeshpipariya <pipariyadharmesh@gmail.com>
2016-10-27 15:59:58 +0530

    update

--- src/lib/tooltip/tooltip.ts
+++ src/lib/tooltip/tooltip.ts
@@ -1,18 +1,22 @@
 import {
+  AfterViewInit,
   ApplicationRef,
+  ChangeDetectorRef,
+  Component,
   ComponentFactoryResolver,
   ComponentRef,
   Directive,
+  ElementRef,
+  HostBinding,
   HostListener,
   Input,
   ReflectiveInjector,
   ViewContainerRef,
+  ViewEncapsulation,
   NgModule,
   ModuleWithProviders
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Md2TooltipComponent } from './tooltip.component';
-import { Md2TooltipOptions } from './tooltip.options';
 
 export type TooltipPosition = 'before' | 'after' | 'above' | 'below';
 
@@ -42,11 +46,6 @@ export class Md2Tooltip {
       return;
     }
     this.visible = true;
-    let options = new Md2TooltipOptions({
-      message: this.message,
-      position: this.position,
-      hostEl: this._viewContainer.element
-    });
 
     clearTimeout(this.timer);
     this.timer = setTimeout(() => {
@@ -54,13 +53,12 @@ export class Md2Tooltip {
       let app: any = this._appRef;
       let appContainer: ViewContainerRef = app['_rootComponents'][0]['_hostElement'].vcRef;
 
-      let providers = ReflectiveInjector.resolve([
-        { provide: Md2TooltipOptions, useValue: <Md2TooltipOptions>options }
-      ]);
-
       let toastFactory = this._componentFactory.resolveComponentFactory(Md2TooltipComponent);
-      let childInjector = ReflectiveInjector.fromResolvedProviders(providers, appContainer.parentInjector);
+      let childInjector = ReflectiveInjector.fromResolvedProviders([], appContainer.parentInjector);
       this.tooltip = appContainer.createComponent(toastFactory, appContainer.length, childInjector);
+      this.tooltip.instance.message = this.message;
+      this.tooltip.instance.position = this.position;
+      this.tooltip.instance.hostEl = this._viewContainer.element;
     }, this.delay);
   }
 
@@ -83,6 +81,117 @@ export class Md2Tooltip {
   }
 }
 
+@Component({
+  moduleId: module.id,
+  selector: 'md2-tooltip',
+  template: `
+    <div class="md2-tooltip-container" [ngStyle]="{top: top, left: left}">
+      <div class="md2-tooltip {{position}}" [class.visible]="_isVisible">{{message}}</div>
+    </div>
+  `,
+  styleUrls: ['tooltip.css'],
+  host: {
+    'role': 'tooltip',
+  },
+  encapsulation: ViewEncapsulation.None
+})
+export class Md2TooltipComponent implements AfterViewInit {
+  private _isVisible: boolean;
+  private top: string = '-1000px';
+  private left: string = '-1000px';
+  message: string;
+  position: string;
+  hostEl: ElementRef;
+
+  constructor(private _element: ElementRef, private _changeDetector: ChangeDetectorRef) {
+    this._isVisible = false;
+  }
+
+  ngAfterViewInit() {
+    let _position = this.positionElements(this.hostEl.nativeElement, this._element.nativeElement.children[0], this.position);
+    this.top = _position.top + 'px';
+    this.left = _position.left + 'px';
+    this._isVisible = true;
+    this._changeDetector.detectChanges();
+  }
+
+  /**
+   * calculate position of target element
+   * @param hostEl host element
+   * @param targetEl targer element
+   * @param position position
+   * @return {top: number, left: number} object of top, left properties
+   */
+  private positionElements(hostEl: HTMLElement, targetEl: HTMLElement, position: string): { top: number, left: number } {
+    let positionStrParts = position.split('-');
+    let pos0 = positionStrParts[0];
+    let pos1 = positionStrParts[1] || 'center';
+    let hostElPos = this.offset(hostEl);
+    let targetElWidth = targetEl.offsetWidth;
+    let targetElHeight = targetEl.offsetHeight;
+    let shiftWidth: any = {
+      center: hostElPos.left + hostElPos.width / 2 - targetElWidth / 2,
+      before: hostElPos.left,
+      after: hostElPos.left + hostElPos.width
+    };
+
+    let shiftHeight: any = {
+      center: hostElPos.top + hostElPos.height / 2 - targetElHeight / 2,
+      above: hostElPos.top,
+      below: hostElPos.top + hostElPos.height
+    };
+
+    let targetElPos: { top: number, left: number };
+    switch (pos0) {
+      case 'before':
+        targetElPos = {
+          top: shiftHeight[pos1],
+          left: (hostElPos.left - targetElWidth)// > 0 ? (hostElPos.left - targetElWidth) : (hostElPos.width + hostElPos.left)
+        };
+        break;
+      case 'after':
+        targetElPos = {
+          top: shiftHeight[pos1],
+          left: shiftWidth[pos0]
+        };
+        break;
+      case 'above':
+        targetElPos = {
+          top: hostElPos.top - targetElHeight,
+          left: shiftWidth[pos1]
+        };
+        break;
+      default:
+        targetElPos = {
+          top: shiftHeight[pos0],
+          left: shiftWidth[pos1]
+        };
+        break;
+    }
+    return targetElPos;
+  }
+
+  /**
+   * calculate offset of target element
+   * @param nativeEl element
+   * @return {width: number, height: number,top: number, left: number} object of with, height, top, left properties
+   */
+  private offset(nativeEl: any): { width: number, height: number, top: number, left: number } {
+    let boundingClientRect = nativeEl.getBoundingClientRect();
+    return {
+      width: boundingClientRect.width || nativeEl.offsetWidth,
+      height: boundingClientRect.height || nativeEl.offsetHeight,
+      top: boundingClientRect.top,
+      left: boundingClientRect.left
+    };
+  }
+
+  private get window(): Window { return window; }
+
+  private get document(): Document { return window.document; }
+
+}
+
 export const MD2_TOOLTIP_DIRECTIVES: any[] = [Md2Tooltip, Md2TooltipComponent];
 
 @NgModule({
@@ -95,9 +204,7 @@ export class Md2TooltipModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: Md2TooltipModule,
-      providers: [{
-        provide: Md2TooltipOptions, useValue: {}
-      }]
+      providers: []
     };
   }
 }
---
+++ tools/gulp/tasks/deploy.ts
@@ -0,0 +1,95 @@
+import * as gulp from 'gulp';
+import * as path from 'path';
+import * as fs from 'fs';
+import gulpRunSequence = require('run-sequence');
+
+const bump = require('gulp-bump');
+const git = require('gulp-git');
+const changelog = require('gulp-conventional-changelog');
+const releaser = require('conventional-github-releaser');
+
+
+gulp.task(':deploy', () => {
+  fs.readFile('./dist/index.html', 'utf8', (err, data) => {
+    if (err) { return console.log(err); }
+    const result = data.replace('<base href="/">', '<base href=".">');
+
+    fs.writeFile('./dist/index.html', result, 'utf8', (err) => {
+      if (err) {
+        return console.log(err);
+      } else {
+        console.log('md2 demo deployed');
+        return gulp.src('./dist/**/*')
+          .pipe(gulp.dest('./deploy'));
+      }
+    });
+  });
+});
+
+// update package.json version
+gulp.task(':release:version', () => {
+  gulp.src(['./package.json'])
+    .pipe(bump({ type: "patch" }))
+    .pipe(gulp.dest('./'));
+  return gulp.src(['./src/lib/package.json'])
+    .pipe(bump({ type: "patch" }))
+    .pipe(gulp.dest('./src/lib'));
+});
+
+// update CHANGELOG.md
+gulp.task(':release:changelog', () => {
+  return gulp.src('CHANGELOG.md', { buffer: false })
+    .pipe(changelog({ preset: 'angular' }))
+    .pipe(gulp.dest('./'));
+});
+
+// release commit
+gulp.task(':release:commit', () => {
+  var version = JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+  return gulp.src('.')
+    .pipe(git.add())
+    .pipe(git.commit('Released MD2@' + version));
+});
+
+// release push
+gulp.task(':release:push', (cb: any) => {
+  git.push('origin', 'master', cb);
+});
+
+// release tag
+gulp.task(':release:tag', (cb: any) => {
+  var version = JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+  git.tag(version, 'Created Tag for version: ' + version, function (error: any) {
+    if (error) { return cb(error); }
+    git.push('origin', 'master', { args: '--tags' }, cb);
+  });
+});
+
+// release
+//gulp.task('github-release', function (done: any) {
+//  releaser({
+//    type: "oauth",
+//    token: '0126af95c0e2d9b0a7c78738c4c00a860b04acc8' // change this to your own GitHub token or use an environment variable
+//  }, {
+//      preset: 'angular' // Or to any other commit message convention you use.
+//    }, done);
+//});
+
+gulp.task('deploy', function (callback: any) {
+  gulpRunSequence(
+    ':deploy',
+    //':release:version',
+    //':release:changelog',
+    //':release:commit',
+    //':release:push',
+    //':release:tag',
+    //'github-release',
+    function (error: any) {
+      if (error) {
+        console.log(error.message);
+      } else {
+        console.log('Md2 Released Successfully');
+      }
+      callback(error);
+    });
+});
