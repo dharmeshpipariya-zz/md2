13ca7095072d480f56afb9c0746c8348182f0e98
dharmeshpipariya <pipariyadharmesh@gmail.com>
2016-11-08 13:02:56 +0530

    update build script

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -1,58 +1,41 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-SOURCE_BRANCH="dev"
+SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Building demo-app"
-    gulp build:devapp
-    exit 0
+  echo "Building demo-app"
+  gulp build:devapp
+  exit 0
 fi
 
 # Save some useful information
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into deploy/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-#git clone $REPO deploy
-echo $REPO
 git clone https://dharmeshpipariya:$GH_TOKEN@github.com/dharmeshpipariya/md2.git --branch=gh-pages deploy
-#cd deploy
-#git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-#cd ..
 
 # Clean deploy existing contents
-#rm -rf deploy/**/* || exit 0
+rm -rf deploy/**/* || exit 0
 
-# Compile demo-app
+# Compile demo.
 gulp build:devapp
 gulp deploy
 
-# Now let's go have some fun with the cloned repo
+# Configure cloned repo.
 cd deploy
 git config --global user.email "pipariyadharmesh@gmail.com"
 git config --global user.name "Dharmesh Pipariya"
 
-# If there are no changes (e.g. this is a README update) then just bail.
+# Check if there are any changes or not.
 if [ -z `git diff --exit-code` ]; then
-    echo "Demo already updated."
-    exit 0
+  echo "Demo already updated."
+  exit 0
 fi
 
-# Commit the "changes", i.e. the new version.
-# The delta will show diffs between new and old versions.
+# Commit changes and Push Demo
 git add -A .
 git commit -m "Update demo: ${SHA}"
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-#openssl aes-256-cbc -K $encrypted_fd9859267a7e_key -iv $encrypted_fd9859267a7e_iv -in deploy_key.enc -out deploy_key -d
-#chmod 600 deploy_key
-#eval `ssh-agent -s`
-#ssh-add deploy_key
-
-# Now that we're all set up, we can push.
-git push origin gh-pages
+git push origin $TARGET_BRANCH
